dc5001408096ce9c728d44a0edf1cd60a260d1bf
Rimon <haqueiftekharul84@gmail.com>
2026-02-26 17:44:42 +0600

    fixed

--- cleanup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "e:\Sem4_1\AI_Lab\Caption_Generator_Project"
-del requirement.txt requirements_clean.txt
-git add -A
-git commit -m "Fix: Use clean requirements.txt for Streamlit Cloud"
-git push
-echo Done!
-pause
